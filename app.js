25b8fef96bae767fe94d98e5f29845367aeb2297
Timur <timur.artyom@gmail.com>
2025-09-10 19:37:35 +0300

    number colors and bit of styling

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 let betNum = document.querySelector('#numPick');
 let btn = document.querySelector('#btn')
 
+// sayı çevirip doğru ve yalnış bilme
 btn.addEventListener('click', () => {
-let randomNum = Math.floor(Math.random() * 10) + 1;
+let randomNum = Math.floor(Math.random() * 10);
 console.log("randomNum")
 if(randomNum === parseInt(betNum.value)){
   console.log("your guess is correct!")
 } else {
   console.log("you guessed wrong")
-}if(parseInt(betNum.value) > 10 || parseInt(betNum.value) === 0){
+}if(parseInt(betNum.value) > 10){
   alert("invalid number")
 }
 })
-// Bizim sayılarımızın göstergelerini tutacak bir container ve içerisine sayılarımız.
+// Sayılarımızın göstergelerini tutacak bir container ve içerisine sayılarımız.
 let numContainer = document.querySelector('#numContainer')
 for(let i = 0; i < 11; i++) {
   let numbers = document.createElement('div')
+  numbers.classList.add('num')
+  numbers.innerText = i
   numContainer.appendChild(numbers)
+  if(parseInt(numbers.innerText) % 2 === 1) {
+  numbers.style.backgroundColor = "red"
+} else if(parseInt(numbers.innerText) === 0) {
+  numbers.style.backgroundColor = "green"
+} else {
+  numbers.style.backgroundColor = "black"
 }
-
-
-
-
+}
